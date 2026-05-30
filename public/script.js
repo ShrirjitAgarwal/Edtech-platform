6c4ade1be2aa004b78215e328a0638b36198c204
Shrirjit Agarwal <shrirjitagarwal@gmail.com>
2026-05-30 12:16:53 +0530

    Remove browser-side eval runner

--- public/script.js
+++ public/script.js
@@ -1,32 +1,10 @@
-console.log("SCRIPT VERSION 3 LOADED");
+console.log("SCRIPT VERSION 4 LOADED");
+
 function go(path){
   window.location.href = path;
 }
 
 function logout(){
-  localStorage.removeItem("role");
+  localStorage.clear();
   window.location.href = "/";
-}
-
-function runCode(id){
-  const code = document.getElementById("code-" + id).value;
-  const output = document.getElementById("output-" + id);
-
-  try {
-    output.innerText = "Running...\n";
-
-    eval(code);
-
-    if(typeof add !== "function"){
-      output.innerText = "Error: Function 'add' not found";
-      return;
-    }
-
-    const result = add(2,3);
-
-    output.innerText = "Running...\nOutput: " + result;
-
-  } catch(err){
-    output.innerText = "Error: " + err.message;
-  }
 }
